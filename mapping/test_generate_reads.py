1805eb7753af4a622809a95d9bcb6b83d061827c
Adam Rabinowitz <adam.rabinowitz@embl.de>
2020-11-23 18:31:01 +0100

    modified tests with quality as a list on integers

--- mapping/test_generate_reads.py
+++ mapping/test_generate_reads.py
@@ -8,7 +8,7 @@ class MissTestErrors(unittest.TestCase):
         with self.assertRaises(AssertionError):
             generate_reads(
                 read_seq='AAAAAAAA',
-                read_qual='01234567',
+                read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
                 read_pos=[1],
                 ref_alleles=[b'A', b'G'],
                 alt_alleles=[b'G', b'C']
@@ -18,7 +18,7 @@ class MissTestErrors(unittest.TestCase):
         with self.assertRaises(AssertionError):
             generate_reads(
                 read_seq='AAAAAAAA',
-                read_qual='01234567',
+                read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
                 read_pos=[1, 8],
                 ref_alleles=[b'A'],
                 alt_alleles=[b'G', b'C']
@@ -28,7 +28,7 @@ class MissTestErrors(unittest.TestCase):
         with self.assertRaises(AssertionError):
             generate_reads(
                 read_seq='AAAAAAAA',
-                read_qual='01234567',
+                read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
                 read_pos=[1, 8],
                 ref_alleles=[b'A', b'G'],
                 alt_alleles=[b'G']
@@ -38,7 +38,7 @@ class MissTestErrors(unittest.TestCase):
         with self.assertRaises(AssertionError):
             generate_reads(
                 read_seq='AAAAAAAA',
-                read_qual='01234567',
+                read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
                 read_pos=[0],
                 ref_alleles=[b'A'],
                 alt_alleles=[b'G']
@@ -48,7 +48,7 @@ class MissTestErrors(unittest.TestCase):
         with self.assertRaises(AssertionError):
             generate_reads(
                 read_seq='AAAAAAAA',
-                read_qual='01234567',
+                read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
                 read_pos=[9],
                 ref_alleles=[b'A'],
                 alt_alleles=[b'G']
@@ -58,7 +58,7 @@ class MissTestErrors(unittest.TestCase):
         with self.assertRaises(AssertionError):
             generate_reads(
                 read_seq='AAAAAAAA',
-                read_qual='01234567',
+                read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
                 read_pos=[8],
                 ref_alleles=[b'AA'],
                 alt_alleles=[b'G']
@@ -68,7 +68,7 @@ class MissTestErrors(unittest.TestCase):
         with self.assertRaises(AssertionError):
             generated_reads = generate_reads(
                 read_seq='AAAAAAAA',
-                read_qual='01234567',
+                read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
                 read_pos=[5, 7],
                 ref_alleles=[b'AAA', b'C'],
                 alt_alleles=[b'GCT', b'T']
@@ -80,61 +80,61 @@ class TestSnpMethods(unittest.TestCase):
     def test_single_reference_snp(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[3],
             ref_alleles=[b'A'],
             alt_alleles=[b'T']
         )
         expected_reads = {
-            ('AATAAAAA', '01234567')
+            ('AATAAAAA', (10, 12, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_single_nonreference_snp(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[3],
             ref_alleles=[b'C'],
             alt_alleles=[b'T']
         )
         expected_reads = {
-            ('AATAAAAA', '01234567'),
-            ('AACAAAAA', '01234567')
+            ('AATAAAAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AACAAAAA', (10, 12, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_double_snp(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[2, 6],
             ref_alleles=[b'C', b'A'],
             alt_alleles=[b'T', b'G']
         )
         expected_reads = {
-            ('ATAAAAAA', '01234567'),
-            ('ACAAAAAA', '01234567'),
-            ('AAAAAGAA', '01234567'),
-            ('ATAAAGAA', '01234567'),
-            ('ACAAAGAA', '01234567')
+            ('ATAAAAAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('ACAAAAAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAGAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('ATAAAGAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('ACAAAGAA', (10, 12, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_double_terminal_snp(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[1, 8],
             ref_alleles=[b'A', b'G'],
             alt_alleles=[b'G', b'C']
         )
         expected_reads = {
-            ('GAAAAAAA', '01234567'),
-            ('AAAAAAAG', '01234567'),
-            ('AAAAAAAC', '01234567'),
-            ('GAAAAAAG', '01234567'),
-            ('GAAAAAAC', '01234567')
+            ('GAAAAAAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAAAG', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAAAC', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('GAAAAAAG', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('GAAAAAAC', (10, 12, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
@@ -143,81 +143,81 @@ class TestLargerNonIndelMethods(unittest.TestCase):
     def test_single_reference_variant(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[3],
             ref_alleles=[b'AA'],
             alt_alleles=[b'TT']
         )
         expected_reads = {
-            ('AATTAAAA', '01234567')
+            ('AATTAAAA', (10, 12, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_single_nonreference_variant(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[7],
             ref_alleles=[b'CG'],
             alt_alleles=[b'TT']
         )
         expected_reads = {
-            ('AAAAAACG', '01234567'),
-            ('AAAAAATT', '01234567')
+            ('AAAAAACG', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAATT', (10, 12, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_multiple_variants(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[2, 7],
             ref_alleles=[b'AAA', b'CG'],
             alt_alleles=[b'GCT', b'TT']
         )
         expected_reads = {
-            ('AGCTAAAA', '01234567'),
-            ('AAAAAACG', '01234567'),
-            ('AAAAAATT', '01234567'),
-            ('AGCTAACG', '01234567'),
-            ('AGCTAATT', '01234567')
+            ('AGCTAAAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAACG', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAATT', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AGCTAACG', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AGCTAATT', (10, 12, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
 
 class TestDeletionMethods(unittest.TestCase):
-    
+
     def test_single_reference_deletion(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[3],
             ref_alleles=[b'AA'],
             alt_alleles=[b'A']
         )
         expected_reads = {
-            ('AAAAAAA', '0124567')
+            ('AAAAAAA', (10, 12, 15, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_single_nonreference_deletion(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[1],
             ref_alleles=[b'TAT'],
             alt_alleles=[b'G']
         )
         expected_reads = {
-            ('TATAAAAA', '01234567'),
-            ('GAAAAA', '134567')
+            ('TATAAAAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('GAAAAA', (12, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_multiple_deletions(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[2, 6],
             ref_alleles=[b'AAA', b'TAC'],
             alt_alleles=[b'TG', b'T']
@@ -229,6 +229,13 @@ class TestDeletionMethods(unittest.TestCase):
             ('ATGATAC', '0224567'),
             ('ATGAT', '02246')
         }
+        expected_reads = {
+            ('ATGAAAA', (10, 14, 14, 18, 20, 22, 24)),
+            ('AAAAATAC', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAT', (10, 12, 14, 16, 18, 22)),
+            ('ATGATAC', (10, 14, 14, 18, 20, 22, 24)),
+            ('ATGAT', (10, 14, 14, 18, 22))
+        }
         self.assertEqual(generated_reads, expected_reads)
 
 class TestInsertionMethods(unittest.TestCase):
@@ -236,44 +243,44 @@ class TestInsertionMethods(unittest.TestCase):
     def test_single_reference_insertion(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[3],
             ref_alleles=[b'A'],
             alt_alleles=[b'TGAT']
         )
         expected_reads = {
-            ('AATGATAAAAA', '01222234567')
+            ('AATGATAAAAA', (10, 12, 14, 14, 14, 14, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_single_nonreference_insertion(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[1],
             ref_alleles=[b'TAT'],
             alt_alleles=[b'GGGCC']
         )
         expected_reads = {
-            ('TATAAAAA', '01234567'),
-            ('GGGCCAAAAA', '1111134567')
+            ('TATAAAAA', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('GGGCCAAAAA', (12, 12, 12, 12, 12, 16, 18, 20, 22, 24))
         }
         self.assertEqual(generated_reads, expected_reads)
 
     def test_multiple_insertions(self):
         generated_reads = generate_reads(
             read_seq='AAAAAAAA',
-            read_qual='01234567',
+            read_qual=[10, 12, 14, 16, 18, 20, 22, 24],
             read_pos=[2, 6],
             ref_alleles=[b'A', b'TAC'],
             alt_alleles=[b'TGG', b'GGCCC']
         )
         expected_reads = {
-            ('ATGGAAAAAA', '0111234567'),
-            ('AAAAATAC', '01234567'),
-            ('AAAAAGGCCC', '0123466666'),
-            ('ATGGAAATAC', '0111234567'),
-            ('ATGGAAAGGCCC', '011123466666'),
+            ('ATGGAAAAAA', (10, 12, 12, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAATAC', (10, 12, 14, 16, 18, 20, 22, 24)),
+            ('AAAAAGGCCC', (10, 12, 14, 16, 18, 22, 22, 22, 22, 22)),
+            ('ATGGAAATAC', (10, 12, 12, 12, 14, 16, 18, 20, 22, 24)),
+            ('ATGGAAAGGCCC', (10, 12, 12, 12, 14, 16, 18, 22, 22, 22, 22, 22)),
         }
         self.assertEqual(generated_reads, expected_reads)
 
