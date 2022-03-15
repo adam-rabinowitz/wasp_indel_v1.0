322d9455be0dea84a37a7abb47f39db0c08c8a24
Adam Rabinowitz <adam.rabinowitz@embl.de>
2022-03-15 14:26:21 +0100

    script now uses CountTree from counts module

--- CHT_input/get_target_regions.py
+++ CHT_input/get_target_regions.py
@@ -1,6 +1,7 @@
 import argparse
 import counts
 import gzip
+import itertools
 
 
 class FilterCounts(object):
@@ -62,7 +63,9 @@ class MultipleBamCounts(object):
             assert(self.chrom_lengths == bam.chrom_lengths)
 
     def get_counts(self, chrom, start, end):
-        counts = [bam.get_counts(chrom, start, end) for bam in self.bam_files]
+        counts = [
+            bam.get_counts(chrom, [start], [end]) for bam in self.bam_files
+        ]
         count_sum = sum(counts)
         return(count_sum)
 
@@ -78,41 +81,58 @@ class MultipleVariantCounts(object):
     ):
         # Store paths and create objects:
         self.paths = paths
-        self.count_files = [counts.VariantCounts(p) for p in self.paths]
+        self.count_trees = [counts.CountTree(p, None) for p in self.paths]
+        # Extract chromosomes
+        chromosomes = [ct.chromosomes for ct in self.count_trees]
+        for chrom in chromosomes[1:]:
+            if chrom != chromosomes[0]:
+                raise ValueError('inconsistent chromosomes')
+        self.chromosomes = chromosomes[0]
         # Set heterozygous haplotypes
         self.heterozygous = ('0|1', '1|0')
 
+    def read_chrom(
+        self, chrom
+    ):
+        # Read variants for chromosome
+        [ct.read_counts(chrom) for ct in self.count_trees]
+
+    def variant_generator(
+        self
+    ):
+        # Loop through chromosome
+        variant_iter = itertools.zip_longest(
+            *[ct.variants for ct in self.count_trees]
+        )
+        for variants in variant_iter:
+            # Check variants
+            for v in variants[1:]:
+                if v != variants[0]:
+                    raise StopIteration('differing variants')
+            # Yield variants
+            yield variants
+
     def get_variants(
-        self, chrom=None, start=None, end=None
+        self, start=None, end=None
     ):
         # Create generators
-        variant_generators = [
-            count_file.get_region_variants(
-                chrom=chrom, start=start, end=end
-            ) for count_file in self.count_files
-        ]
+        variant_iter = itertools.zip_longest(
+            *[ct.get_variants([start], [end]) for ct in self.count_trees]
+        )
         # Loop through generators and get variants
-        while True:
-            # Get next variants
-            try:
-                variants = [
-                    generator.__next__() for
-                    generator in variant_generators
-                ]
-            except StopIteration:
-                break
-            # Check identity and yield
-            for variant in variants[1:]:
-                assert(variant == variants[0])
-            yield(variants)
+        for variants in variant_iter:
+            # Check variants
+            for v in variants[1:]:
+                if v != variants[0]:
+                    raise StopIteration('differing variants')
+            # Yield variants
+            yield variants
 
-    def get_as_count(self, chrom, start, end):
+    def get_as_count(self, start, end):
         # Set output allele specific counts
         as_count = 0
         # Loop through all region variants in all count files
-        for variants in self.get_variants(
-            chrom=chrom, start=start, end=end
-        ):
+        for variants in self.get_variants(start=start, end=end):
             for variant in variants:
                 # Add ref and alt counts if variant is heterozygous
                 if variant.haplotype in self.heterozygous:
@@ -120,10 +140,6 @@ class MultipleVariantCounts(object):
                     as_count += variant.alt_as_count
         # Return allele specific counts
         return(as_count)
-
-    def close(self):
-        for count_file in self.count_files:
-            count_file.close()
 
 
 class FilterVariants(object):
@@ -144,7 +160,6 @@ class FilterVariants(object):
         self
     ):
         self.bam_counts.close()
-        self.variant_counts.close()
 
     def region_generator(
         self, path
@@ -170,6 +185,21 @@ class FilterVariants(object):
         # Close file
         infile.close()
 
+    def count_hets(
+        self, variants
+    ):
+        haplotypes = [v.haplotype for v in variants]
+        het_count = haplotypes.count('0|1') + haplotypes.count('1|0')
+        return(het_count)
+
+    def minor_as_count(
+        self, variants
+    ):
+        ref_as_count = sum([v.ref_as_count for v in variants])
+        alt_as_count = sum([v.alt_as_count for v in variants])
+        minor_as_count = min(ref_as_count, alt_as_count)
+        return(minor_as_count)
+
     def filter_windows(
         self, min_het, min_minor, min_as_reads, min_total_reads, window,
         outpath
@@ -181,56 +211,51 @@ class FilterVariants(object):
             outfile = open(outpath, 'wt')
         # Create counter
         filter_counts = FilterCounts()
-        # Loop through all variants
-        for test_variants in self.variant_counts.get_variants():
-            filter_counts.variants += 1
-            # Skip test variants if heterozygous count is too low
-            test_haplotypes = [tv.haplotype for tv in test_variants]
-            het_count = (
-                test_haplotypes.count('0|1') + test_haplotypes.count('1|0')
-            )
-            if het_count < min_het:
-                filter_counts.low_het += 1
-                continue
-            # Skip test variants if minor allele count is too low
-            ref_as_count = sum([tv.ref_as_count for tv in test_variants])
-            alt_as_count = sum([tv.alt_as_count for tv in test_variants])
-            minor_as_count = min(ref_as_count, alt_as_count)
-            if minor_as_count < min_minor:
-                filter_counts.low_minor += 1
-                continue
-            # Count passed variants and considered regions
-            filter_counts.variants_passed += 1
-            filter_counts.regions += 1
-            # Define region
-            chrom = test_variants[0].chrom
-            chrom_length = self.bam_counts.chrom_lengths[chrom]
-            region_start = max(test_variants[0].start - window, 0)
-            region_end = min(test_variants[0].end + window, chrom_length)
-            # Skip region if total reads is too low
-            total_reads = self.bam_counts.get_counts(
-                chrom=chrom, start=region_start, end=region_end
-            )
-            if total_reads < min_total_reads:
-                filter_counts.low_total_reads += 1
-                continue
-            # Skip region if allele specific reads are too low
-            as_reads = self.variant_counts.get_as_count(
-                chrom=chrom, start=region_start, end=region_end
-            )
-            if as_reads < min_as_reads:
-                filter_counts.low_as_reads += 1
-                continue
-            # Write acceptable test variants and regions to file
-            filter_counts.regions_passed += 1
-            outline = (
-                '{chrom}\t{pos}\t{ref}\t{alt}\t{start}\t{end}\n'
-            ).format(
-                chrom=chrom, pos=test_variants[0].start + 1,
-                ref=test_variants[0].ref, alt=test_variants[0].alt,
-                start=region_start + 1, end=region_end
-            )
-            outfile.write(outline)
+        # Loop through chromosome
+        for chrom in self.variant_counts.chromosomes:
+            self.variant_counts.read_chrom(chrom)
+            # Loop through variants
+            for test_variants in self.variant_counts.variant_generator():
+                filter_counts.variants += 1
+                # Skip test variants if heterozygous count is too low
+                if self.count_hets(test_variants) < min_het:
+                    filter_counts.low_het += 1
+                    continue
+                # Skip test variants if minor allele count is too low
+                if self.minor_as_count(test_variants) < min_minor:
+                    filter_counts.low_minor += 1
+                    continue
+                # Count passed variants and considered regions
+                filter_counts.variants_passed += 1
+                filter_counts.regions += 1
+                # Define region
+                chrom_length = self.bam_counts.chrom_lengths[chrom]
+                region_start = max(test_variants[0].start - window, 0)
+                region_end = min(test_variants[0].end + window, chrom_length)
+                # Skip region if total reads is too low
+                total_reads = self.bam_counts.get_counts(
+                    chrom=chrom, start=region_start, end=region_end
+                )
+                if total_reads < min_total_reads:
+                    filter_counts.low_total_reads += 1
+                    continue
+                # Skip region if allele specific reads are too low
+                as_reads = self.variant_counts.get_as_count(
+                    start=region_start, end=region_end
+                )
+                if as_reads < min_as_reads:
+                    filter_counts.low_as_reads += 1
+                    continue
+                # Write acceptable test variants and regions to file
+                filter_counts.regions_passed += 1
+                outline = (
+                    '{chrom}\t{pos}\t{ref}\t{alt}\t{start}\t{end}\n'
+                ).format(
+                    chrom=chrom, pos=test_variants[0].start + 1,
+                    ref=test_variants[0].ref, alt=test_variants[0].alt,
+                    start=region_start + 1, end=region_end
+                )
+                outfile.write(outline)
         # Print filter metrics
         print(filter_counts.variant_counts())
         print(filter_counts.region_counts())
@@ -246,10 +271,22 @@ class FilterVariants(object):
             outfile = open(outpath, 'wt')
         # Create counter
         filter_counts = FilterCounts()
+        # Set current chromosome
+        current_chrom = None
+        previous_chroms = []
         # Loop thorugh region file
         reg_gen = self.region_generator(regions)
         for chrom, test_start, test_end, region_start, region_end in reg_gen:
             filter_counts.regions += 1
+            # Check chromosome
+            if chrom != current_chrom:
+                # Raise error if chromosome has been seen before
+                if chrom in previous_chroms:
+                    raise ValueError('region file must chromosome sorted')
+                # Store new chromosome and read variants
+                previous_chroms.append(chrom)
+                current_chrom = chrom
+                self.variant_counts.read_chrom(chrom)
             # Adjust stars to account for zero indexing
             test_start -= 1
             region_start -= 1
@@ -262,7 +299,7 @@ class FilterVariants(object):
                 continue
             # Skip region if allele specific reads are too low
             as_reads = self.variant_counts.get_as_count(
-                chrom=chrom, start=region_start, end=region_end
+                start=region_start, end=region_end
             )
             if as_reads < min_as_reads:
                 filter_counts.low_as_reads += 1
@@ -270,22 +307,15 @@ class FilterVariants(object):
             # Loop through variants
             filter_counts.regions_passed += 1
             for test_variants in self.variant_counts.get_variants(
-                chrom=chrom, start=test_start, end=test_end
+                start=test_start, end=test_end
             ):
                 filter_counts.variants += 1
                 # Skip test variants if heterozygous count is too low
-                test_haplotypes = [tv.haplotype for tv in test_variants]
-                het_count = (
-                    test_haplotypes.count('0|1') + test_haplotypes.count('1|0')
-                )
-                if het_count < min_het:
+                if self.count_hets(test_variants) < min_het:
                     filter_counts.low_het += 1
                     continue
                 # Skip test variants if minor allele count is too low
-                ref_as_count = sum([tv.ref_as_count for tv in test_variants])
-                alt_as_count = sum([tv.alt_as_count for tv in test_variants])
-                minor_as_count = min(ref_as_count, alt_as_count)
-                if minor_as_count < min_minor:
+                if self.minor_as_count(test_variants) < min_minor:
                     filter_counts.low_minor += 1
                     continue
                 # Write acceptable test variants and regions to file
@@ -385,5 +415,4 @@ if __name__ == "__main__":
             min_total_reads=args.min_total_reads,
             regions=args.regions, outpath=args.outfile
         )
-    # Close files
     variant_filter.close()
